4e03300c747b219b1ff0dd612df6587ac2a269c2
janghs <gorae930301@gmail.com>
2024-05-20 15:06:12 +0900

    KafkaStream으로 ERROR 로그 다른 Topic에 전달

---
+++ src/main/java/com/example/kafkadashboard/config/KafkaStreamConfig.java
@@ -0,0 +1,45 @@
+package com.example.kafkadashboard.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.ValueMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
+import org.springframework.kafka.config.KafkaStreamsConfiguration;
+
+@Configuration
+@EnableKafkaStreams
+public class KafkaStreamConfig {
+
+	// Spring Kafka가 기본 설정을 찾을 수 있도록 빈 이름을 명시적으로 지정
+    @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
+    public KafkaStreamsConfiguration kStreamsConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-stream-application");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        return new KafkaStreamsConfiguration(props);
+    }
+
+    @Bean
+    public KStream<String, String> kStream(StreamsBuilder streamsBuilder) {
+        KStream<String, String> stream = streamsBuilder.stream("test-logs");
+
+//        Kafka Stream 테스트
+//        stream = stream.mapValues((ValueMapper<? super String, ? extends String>) String::toUpperCase);
+//        stream.to("output-topic");
+        
+        // ERROR 로그는 output-topic으로 별도로 보낸다.
+        stream.filter((k, v) -> v.contains("ERROR")).to("output-topic");
+        
+        return stream;
+    }
+}
